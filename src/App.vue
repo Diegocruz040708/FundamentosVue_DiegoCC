85891f3236865df639963c6f520998b9415e6587
Diegocruz040708 <cruzdieg040708@gmail.com>
2025-03-22 20:45:43 -0600

    Agregando Lista de videojuegos

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <script setup>
 import Navbar from './components/Navbar.vue';
 import Carrusel from './components/Carrusel.vue';
+import ListaV from './components/ListaV.vue';
 </script>
 
 <template lang>
 <Navbar />
-<Carrusel />
+<div class="row">
+    <Carrusel />
+    <ListaV />
+</div>
+
 </template>
 
 <style scoped>
