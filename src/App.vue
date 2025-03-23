22483cf28d33142422e577d6a5f866ec41576c69
Diegocruz040708 <cruzdieg040708@gmail.com>
2025-03-22 19:45:17 -0600

    Agregando imagenes y carrusel

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
 import Navbar from './components/Navbar.vue';
-
+import Carrusel from './components/Carrusel.vue';
 </script>
 
 <template lang>
 <Navbar />
+<Carrusel />
 </template>
 
 <style scoped>
---
+++ src/components/Carrusel.vue
@@ -0,0 +1,62 @@
+<script setup>
+
+</script>
+<template>
+      <div class="container">
+   
+   <h1>Bienvenido a nuestra página de Videojuegos</h1>
+   <p>
+     "Descubre los videojuegos más populares, adquiere tus títulos preferidos
+     y mantente actualizado con las últimas novedades del mundo gamer.
+     ¡Sumérgete en juegos como Fortnite, Apex Legends, Call of Duty y muchos
+     más!"
+   </p>
+ </div>
+      <!-- Inicio del col-md-6 para los juegos mas populares -->
+      <div class="col-md-6">
+      <div class="card mt">
+        <div style="background-color: #E94560;" class="card-header">
+          <h1 style="color: beige; font-size: 40px; font-family: 'Open Sans', sans-serif; text-transform: uppercase; letter-spacing: 3px; text-shadow: 3px 3px 10px rgba(0, 0, 0, 0.5);">  
+                VIDEOJUEGOS MÁS POPULARES  
+            </h1>
+        </div>
+        <div class="card-body">
+          <!-- inicio de carrusel de reproduccion automatico -->
+          <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="/public/fortnite.jpg" class="d-block w-100" alt="Videojuego 1">
+              </div>
+              <div class="carousel-item">
+                <img src="/public/Apex.jpg" class="d-block w-100" alt="Videojuego 2">
+              </div>
+              <div class="carousel-item">
+                <img src="/public/pubg.jpg" class="d-block w-100" alt="Videojuego 3">
+              </div>
+              <div class="carousel-item">
+                <img src="/public/Gears.jpg" class="d-block w-100" alt="Videojuego 4">
+              </div>
+              <div class="carousel-item">
+                <img src="/public/COD.jpg" class="d-block w-100" alt="Videojuego 5">
+              </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
+              data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying"
+              data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+            <!-- fin del carrusel de reproduccion automatico -->
+          </div>
+        </div>
+      </div>
+    </div>  
+</template>
+
+<style scoped>
+
+</style>
